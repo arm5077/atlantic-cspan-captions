63cc1bfc54f30f5b9707b27edac2500acda9678f
Andrew McGill <arm5077@gmail.com>
2016-07-26 15:39:17 -0400

    add front end

--- public/app.js
+++ public/app.js
@@ -1,12 +1,48 @@
 angular.module("app", [])
 .controller("controller", ["$scope", "$http", "$sce", function($scope, $http, $sce){
+
+  $scope.renderHTML = function(text){ return $sce.trustAsHtml(text); };
+
   var socket = io();
+  $scope.transcript = [];
+  
+  $scope.text = "";
+  var sentence = "";
+  var time = moment().format("h:mm a, MMM. D");
+  var speaker = "Unknown";
+
 
   socket.on('word', function(data) {
-    console.log("bobobobo");
-    console.log(data.data.body);
-    words = words + data.data.body + " ";
-  });
+    var word = data.data.body;
+    console.log( word );
+    
+    $scope.$apply(function(){
+      // If we reach a chevron start a new section block
+      if( word.indexOf(">>") != -1 ){
+        $scope.transcript.unshift({ speaker: speaker, time: time, text: $scope.text });
+        $scope.text = "";
+        time = moment().format("H:mm a, MMM. D");
+        console.log($scope.transcript);
+      }
+      else if( word.indexOf(":") != -1){
+        $scope.text = $scope.text + "<strong>" + sentence + word + "</strong> ";
+        sentence = ""
+      }
+      else if( word.indexOf(".") != -1 || word.indexOf("?") != -1 || word.indexOf("!") != -1 ){
+        sentence = sentence + data.data.body + " ";
 
-	
+        $scope.text = $scope.text + capitalizeFirstLetter(sentence.toLowerCase()); + " ";
+        sentence = "";
+      }
+      else {
+        sentence = sentence + word + " ";
+      }
+    });
+  });
+  
 }]);
+
+// Thanks http://stackoverflow.com/questions/1026069/capitalize-the-first-letter-of-string-in-javascript
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
